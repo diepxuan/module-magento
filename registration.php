ed548d25ea959b5ac3ec8838ba3286162b6f93b3
diepxuan <ductn@diepxuan.com>
2024-06-29 09:16:45 +0000

    new module for multi store

--- registration.php
+++ registration.php
@@ -1,7 +1,20 @@
 <?php
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-06-29 16:13:04
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
     'Diepxuan_Magento',
     __DIR__
 );
